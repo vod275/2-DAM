8222be1e475c0be64b58b9f1dab1ae4ab9e3c3c4
vod275 <138567072+vod275@users.noreply.github.com>
2024-09-26 20:31:31 +0200

    AD26sep

--- Acceso a datos/LeerXML/src/main/java/LeerXML/LeerXML.java
+++ Acceso a datos/LeerXML/src/main/java/LeerXML/LeerXML.java
@@ -3,7 +3,6 @@ package LeerXML;
 import java.io.File;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 
 public class LeerXML {
@@ -16,13 +15,26 @@ public class LeerXML {
             // Unmarshalling: Leer XML y convertirlo en un objeto Java
             Unmarshaller unmarshaller = context.createUnmarshaller();
             VentaArticulos ventasArticulos = (VentaArticulos) unmarshaller.unmarshal(new File("ventasarticulos.xml"));
-            
-            // Manipular los datos si es necesario
 
-            // Marshalling: Convertir el objeto Java de nuevo a XML
-            Marshaller marshaller = context.createMarshaller();
-            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);  // Para formatear el XML de salida
-            marshaller.marshal(ventasArticulos, System.out);  // Mostrar el XML en consola
+            // Mostrar el artículo
+            Articulo articulo = ventasArticulos.getArticulo();
+            System.out.println("Artículo:");
+            System.out.println("Código: " + articulo.getCodigo());
+            System.out.println("Denominación: " + articulo.getDenominacion());
+            System.out.println("Stock: " + articulo.getStock());
+            System.out.println("Precio: " + articulo.getPrecio());
+            System.out.println();
+
+            // Mostrar todas las ventas
+            Ventas ventas = ventasArticulos.getVentas();
+            System.out.println("Ventas:");
+            for (Venta venta : ventas.getVenta()) {
+                System.out.println("Número de Venta: " + venta.getNumVenta());
+                System.out.println("Unidades: " + venta.getUnidades());
+                System.out.println("Nombre del Cliente: " + venta.getNombreCliente());
+                System.out.println("Fecha: " + venta.getFecha());
+                System.out.println("__________________________");
+            }
 
         } catch (JAXBException e) {
             e.printStackTrace();
