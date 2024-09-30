54dcac0dcb464d25cfdcdf0cc41fa2ec55f034a2
vod275 <138567072+vod275@users.noreply.github.com>
2024-09-30 19:57:51 +0200

    AD30sep

---
+++ Acceso a datos/PruebaXSD_XML/src/main/java/ClasesVentas/ObjectFactory.java
@@ -0,0 +1,88 @@
+//
+// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.3.2 
+// Visite <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
+// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
+// Generado el: 2024.09.30 a las 07:27:31 PM CEST 
+//
+
+
+package ClasesVentas;
+
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.annotation.XmlElementDecl;
+import javax.xml.bind.annotation.XmlRegistry;
+import javax.xml.namespace.QName;
+
+
+/**
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the ClasesVentas package. 
+ * <p>An ObjectFactory allows you to programatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
+ * provided in this class.
+ * 
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+    private final static QName _Ventasarticulos_QNAME = new QName("", "ventasarticulos");
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: ClasesVentas
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link Ventas }
+     * 
+     */
+    public Ventas createVentas() {
+        return new Ventas();
+    }
+
+    /**
+     * Create an instance of {@link VentasType }
+     * 
+     */
+    public VentasType createVentasType() {
+        return new VentasType();
+    }
+
+    /**
+     * Create an instance of {@link DatosArtic }
+     * 
+     */
+    public DatosArtic createDatosArtic() {
+        return new DatosArtic();
+    }
+
+    /**
+     * Create an instance of {@link Ventas.Venta }
+     * 
+     */
+    public Ventas.Venta createVentasVenta() {
+        return new Ventas.Venta();
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link VentasType }{@code >}
+     * 
+     * @param value
+     *     Java instance representing xml element's value.
+     * @return
+     *     the new instance of {@link JAXBElement }{@code <}{@link VentasType }{@code >}
+     */
+    @XmlElementDecl(namespace = "", name = "ventasarticulos")
+    public JAXBElement<VentasType> createVentasarticulos(VentasType value) {
+        return new JAXBElement<VentasType>(_Ventasarticulos_QNAME, VentasType.class, null, value);
+    }
+
+}
---
+++ Acceso a datos/PruebaXSD_XML/src/main/java/ClasesVentas/VisualizarXML.java
@@ -0,0 +1,131 @@
+package ClasesVentas;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+
+public class VisualizarXML {
+
+	public static void main(String[] args) {
+		
+		visualizarxml();
+
+	}
+
+	
+	public static void visualizarxml() { 
+		  System.out.println("------------------------------ "); 
+		  System.out.println("-------VISUALIZAR XML--------- "); 
+		  System.out.println("------------------------------ "); 
+		  try { 
+		      //Creamos el contexto 
+		      JAXBContext jaxbContext =      
+		          JAXBContext.newInstance(ObjectFactory.class); 
+		 
+		  Unmarshaller u = jaxbContext.createUnmarshaller();  
+		 
+		 JAXBElement jaxbElement = (JAXBElement) u.unmarshal 
+		             (new FileInputStream("./ventasarticulos.xml")); 
+		 
+		 Marshaller m = jaxbContext.createMarshaller(); 
+		 
+		 m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE); 
+		 
+		 // Visualiza por consola 
+		 m.marshal(jaxbElement, System.out); 
+		       
+		      //Cargamos ahora el documento en los tipos 
+		 VentasType miventa = (VentasType) jaxbElement.getValue(); 
+		 
+		 //Obtenemos una instancia para obtener todas las ventas 
+		      Ventas vent = miventa.getVentas(); 
+		 
+		  // Guardamos las ventas en la lista 
+		 List listaVentas = new ArrayList(); 
+		 listaVentas = vent.getVenta(); 
+		 
+		 System.out.println("---------------------------- "); 
+		 System.out.println("---VISUALIZAR LOS OBJETOS--- "); 
+		 System.out.println("---------------------------- "); 
+		 // Cargamos los datos del artículo 
+		 DatosArtic miartic = (DatosArtic) miventa.getArticulo(); 
+		 System.out.println("Nombre art: " +  
+		               miartic.getDenominacion()); 
+		 System.out.println("Coodigo art: " + miartic.getCodigo()); 
+		 System.out.println("Ventas  del artículo , hay: " +  
+		          listaVentas.size()); 
+		      //Visualizamos las ventas del artículo 
+		 for (int i = 0; i < listaVentas.size(); i++) { 
+		  Ventas.Venta ve = (Ventas.Venta) listaVentas.get(i); 
+		  System.out.println("Número de venta: " + 
+		     ve.getNumventa() + ". Nombre cliente: " +  
+		     ve.getNombrecliente() + ", unidades: " +  
+		     ve.getUnidades() + ", fecha: " + ve.getFecha()); 
+		       } 
+		   } catch (JAXBException je) { 
+		   System.out.println(je.getCause()); 
+		   } catch (IOException ioe) { 
+		   System.out.println(ioe.getMessage());} 
+		}
+	
+	
+	private static void insertarventa (int numeventa, String nomcli, int uni, String fecha) { 
+	  System.out.println("---------------------------- "); 
+	  System.out.println("-------AÑADIR VENTA--------- "); 
+	  System.out.println("---------------------------- "); 
+	  try { 
+	     JAXBContext jaxbContext =    
+	             JAXBContext.newInstance(ObjectFactory.class); 
+	      Unmarshaller u = jaxbContext.createUnmarshaller(); 
+	      JAXBElement jaxbElement = (JAXBElement)  
+	     u.unmarshal(new FileInputStream("./ventasarticulos.xml")); 
+	 
+	      VentasType miventa = (VentasType) jaxbElement.getValue(); 
+	 
+	      Ventas vent = miventa.getVentas(); 
+	 
+	      List listaVentas = new ArrayList(); 
+	      listaVentas = vent.getVenta(); 
+	 
+	      // comprobar si existe el número de venta,  
+	     // reccorriendo el arraylist 
+	      int existe = 0;  
+	      for (int i = 0; i < listaVentas.size(); i++) { 
+	    Ventas.Venta ve = (Ventas.Venta) listaVentas.get(i); 
+	    if (ve.getNumventa().intValue() == numeventa) { 
+	   existe = 1; break;} 
+	      } 
+	      if (existe == 0) { 
+	        // Crear el objeto Ventas.Venta 
+	        Ventas.Venta ve = new Ventas.Venta(); 
+	        ve.setNombrecliente(nomcli); 
+	        ve.setFecha(fecha); ve.setUnidades(uni); 
+	        BigInteger nume = BigInteger.valueOf(numeventa); 
+	        ve.setNumventa(nume); 
+	       // Se añade la venta a la lista 
+	       listaVentas.add(ve); 
+	       //Se crea el Marshaller, volcar la lista al fichero XML 
+	        Marshaller m = jaxbContext.createMarshaller(); 
+	        m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT,  
+	             Boolean.TRUE);  
+	       m.marshal(jaxbElement, new  
+	              FileOutputStream("./ventasarticulos.xml")); 
+	       System.out.println("Venta añadida: " + numeventa); 
+	    } else 
+	        System.out.println("En número de venta ya existe: " +  
+	           numeventa); 
+	  } catch (JAXBException je) { 
+	  System.out.println(je.getCause()); 
+	  } catch (IOException ioe) { 
+	  System.out.println(ioe.getMessage());} 
+	 
+	} 
+
+}
