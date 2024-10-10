462ae0f3f33a25debe66e96c7d6965e5a9187b89
vod275 <138567072+vod275@users.noreply.github.com>
2024-10-10 20:57:02 +0200

    AD10oct

--- Acceso a datos/EjercicioExamen2/src/main/java/empresas.java
+++ Acceso a datos/EjercicioExamen2/src/main/java/empresas.java
@@ -1,4 +1,22 @@
+import java.util.ArrayList;
 
 public class empresas {
 
+	ArrayList<empresa> empresa;
+
+	public empresas(ArrayList<empresa> empresa) {
+		super();
+		this.empresa = empresa;
+	}
+	public empresas() {
+		
+	}
+
+	public ArrayList<empresa> getEmpresa() {
+		return empresa;
+	}
+
+	public void setEmpresa(ArrayList<empresa> empresa) {
+		this.empresa = empresa;
+	}
 }
