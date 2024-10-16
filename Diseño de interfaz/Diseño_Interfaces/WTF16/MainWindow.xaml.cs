5895b5ab06dec71b81d6718a4da198378b63ed5f
vod275 <138567072+vod275@users.noreply.github.com>
2024-10-16 18:17:03 +0200

    DI16oct

--- Diseño de interfaz/Diseño_Interfaces/WTF16/MainWindow.xaml.cs
+++ Diseño de interfaz/Diseño_Interfaces/WTF16/MainWindow.xaml.cs
@@ -25,5 +25,58 @@ namespace WTF16
         {
             Close();
         }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            
+            if (string.IsNullOrWhiteSpace(tbNombre.Text))
+            {
+                MessageBox.Show("Debe ingresar una profesión.", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(tbHermanos.Text) || !int.TryParse(tbHermanos.Text, out int numeroHermanos))
+            {
+                MessageBox.Show("Debe ingresar un número válido de hermanos.", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            if (cbEdad.SelectedItem == null)
+            {
+                MessageBox.Show("Debe seleccionar una edad.", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            if (!(rbMasculino.IsChecked == true || rbFemenino.IsChecked == true))
+            {
+                MessageBox.Show("Debe seleccionar un sexo.", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+
+
+            var nuevaPersona = new Persona
+            {
+                Profesion = tbNombre.Text,
+                NumeroHermanos = numeroHermanos,
+                Edad = ((ComboBoxItem)cbEdad.SelectedItem).Content.ToString(),
+                Sexo = rbMasculino.IsChecked == true ? "Masculino" : "Femenino",
+                if( cbPractica.IsChecked == true)
+                 {
+                    DeporteQuePractica = lbDeporte.SelectedItem != null ? ((ListBoxItem)lbDeporte.SelectedItem).Content.ToString();
+
+                 }
+                else
+                {
+                    DeporteQuePractica  = Console.WriteLine("No hace deporte");
+                };
+
+                ,
+                AficionCompras = (int)slCompras.Value,
+                AficionTelevision = (int)slTelevision.Value,
+                AficionCine = (int)slCine.Value
+            };
+
+        }
     }
 }
