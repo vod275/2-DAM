94ed3ebf2583127151660c95f475af067fed8b71
vod275 <138567072+vod275@users.noreply.github.com>
2024-10-16 19:11:59 +0200

    Android16oct

---
+++ Android/Tema 1/Dialogos/app/src/main/java/com/example/dialogos/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.dialogos
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.dialogos.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.btAlertaSimple.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Alerta")
+            builder.setMessage("Esto es una alerta")
+            builder.setPositiveButton("Aceptar") { dialog, which ->
+                // Acciones a realizar al hacer clic en "Aceptar"
+            }
+            val dialog = builder.create()
+            dialog.show()
+
+        }
+    }
+
+
+
+}
