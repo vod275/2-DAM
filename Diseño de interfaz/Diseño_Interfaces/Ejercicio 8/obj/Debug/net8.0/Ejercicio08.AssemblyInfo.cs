66012688f1bd8faa47b7d1c63045462de3e3459c
vod275 <138567072+vod275@users.noreply.github.com>
2024-10-30 11:26:56 +0100

    DI30oct

--- Diseño de interfaz/Diseño_Interfaces/Ejercicio 8/obj/Debug/net8.0/Ejercicio08.AssemblyInfo.cs
+++ Diseño de interfaz/Diseño_Interfaces/Ejercicio 8/obj/Debug/net8.0/Ejercicio08.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ejercicio08")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c2f084bfdc9c2ce2421d903ecd6aa50457cfdf0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06585b95777adf15420cb0c1560a6f0fee41fd6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ejercicio08")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ejercicio08")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Diseño de interfaz/Diseño_Interfaces/WTF05/obj/Debug/net8.0-windows/MainWindow.g.i.cs
+++ Diseño de interfaz/Diseño_Interfaces/WTF05/obj/Debug/net8.0-windows/MainWindow.g.i.cs
@@ -85,7 +85,7 @@ namespace WTF05 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WTF05;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WTF05_Coversion_Monedas;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
