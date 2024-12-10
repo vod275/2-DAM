84cb58bec607a67ae824f3329ef81f160a1bc70a
vod275 <vod27577@hotmail.com>
2024-12-10 20:20:43 +0100

    AD10Dic

--- Acceso_a_datos/BBDDEjercicios/src/main/java/hybernate/EjemploHibernate.java
+++ Acceso_a_datos/BBDDEjercicios/src/main/java/hybernate/EjemploHibernate.java
@@ -10,37 +10,37 @@ import hibernateClases.Departamentos;
 
 public class EjemploHibernate {
 
-		public static void main(String[] args) {
-		 // Inicializa el entorno Hibernate
-		 Configuration cfg = new Configuration().configure();
-		 // Crea el ejemplar de sesion factory (fabrica de sesiones)
-		 SessionFactory sessionFactory = cfg.buildSessionFactory();
-		 // Obtiene un objeto session
-		 Session sesion = sessionFactory.openSession();
-		 Transaction tx =sesion.beginTransaction();
-		 System.out.println("Inserto una fila en la tabla DEPARTAMENTOS." );
-		 Departamentos dep = new Departamentos();
-		 dep.setDeptNo((byte) 62);
-		 dep.setDnombre("MARKETING");
-		 dep.setLoc("GUADALAJARA");
-		 try {
-		sesion.save(dep);
-		tx.commit();
-		 System.out.println("REGISTRO GRABADO ");
-		}catch (JDBCException j)
-		{
-		 System.out.println("Codigo error: "+j.getErrorCode());
-		 System.out.println("Mensaje : "+j.getMessage() );
-		 //1406 Datos sobrepasan long de la columna
-		 //1062 Clave duplicada
+	public static void main(String[] args) {
+		// Inicializa el entorno Hibernate
+		Configuration cfg = new Configuration().configure();
+		
+		// Crea el ejemplar de sesion factory (fabrica de sesiones)
+		SessionFactory sessionFactory = cfg.buildSessionFactory();
+		
+		// Obtiene un objeto session
+		Session sesion = sessionFactory.openSession();
+		Transaction tx = sesion.beginTransaction();
+		System.out.println("Inserto una fila en la tabla DEPARTAMENTOS.");
+		Departamentos dep = new Departamentos();
+		dep.setDeptNo((byte) 62);
+		dep.setDnombre("MARKETING");
+		dep.setLoc("GUADALAJARA");
+		
+		
+		try {
+			sesion.persist(dep);
+			tx.commit();
+			System.out.println("REGISTRO GRABADO ");
+		} catch (JDBCException j) {
+			System.out.println("Codigo error: " + j.getErrorCode());
+			System.out.println("Mensaje : " + j.getMessage());
+			// 1406 Datos sobrepasan long de la columna
+			// 1062 Clave duplicada
+		} catch (Exception e) {
+			System.out.println("Codigo error: " + e.hashCode());
+			System.out.println("Mensaje : " + e.getMessage());
 		}
-		catch(Exception e)		
-		{
-		 System.out.println("Codigo error: "+e.hashCode() );
-		 System.out.println("Mensaje : "+e.getMessage() );
-		}
-		 sesion.close();
-		}
-
+		sesion.close();
+	}
 
 }
