dac082392ac3b166555fdd3e11cf188435769739
vod275 <vod27577@hotmail.com>
2024-12-17 21:10:24 +0100

    DIESTILOS

--- Diseño de interfaz/Diseño_Interfaces/ViewModelAdivinar/obj/Debug/net8.0-windows/MainWindow.g.i.cs
+++ Diseño de interfaz/Diseño_Interfaces/ViewModelAdivinar/obj/Debug/net8.0-windows/MainWindow.g.i.cs
@@ -109,7 +109,7 @@ namespace ViewModelAdivinar {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/ViewModelAdivinar;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/ViewModelAdivinar;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- Diseño de interfaz/Diseño_Interfaces/ViewModelBarra/obj/Debug/net8.0-windows/ViewModelBarra.AssemblyInfo.cs
+++ Diseño de interfaz/Diseño_Interfaces/ViewModelBarra/obj/Debug/net8.0-windows/ViewModelBarra.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ViewModelBarra")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9124f487bd152e58fdeea68c0a410b1dbb9bca5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1579ba00988da11fed8b935c03af096b693eedd8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ViewModelBarra")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ViewModelBarra")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
